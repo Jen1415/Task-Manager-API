64bf01326b4931ef4c6790da3c5c34a1d3e2e7bb
Jen1415 <qiwen894@gmail.com>
2026-09-02 11:19:05 +0800

    feat: alembic migration set up and first migration run

---
+++ src/taskmanagerapi/models/__init__.py
@@ -0,0 +1,3 @@
+# src/taskmanagerapi/models/__init__.py
+from taskmanagerapi.models.user import User
+from taskmanagerapi.models.task import Task
--- src/taskmanagerapi/models/task.py
+++ src/taskmanagerapi/models/task.py
@@ -1,15 +1,6 @@
 import enum
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey, Enum as SQLEnum
-from sqlalchemy.orm import relationship
 from taskmanagerapi.database import Base
-
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
 
 class TaskStatus(str, enum.Enum):
     todo = "todo"
